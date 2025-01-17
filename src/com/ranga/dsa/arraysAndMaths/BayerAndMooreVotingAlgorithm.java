62ac8988aa26615552bbbe582125d88b0cdb8e08
rangareddy <rangareddyjava9@gmail.com>
2025-01-17 21:28:04 +0530

    BayerMore Voting Algorithm

---
+++ src/com/ranga/dsa/arraysAndMaths/BayerAndMooreVotingAlgorithm.java
@@ -0,0 +1,36 @@
+package com.ranga.dsa.arraysAndMaths;
+
+public class BayerAndMooreVotingAlgorithm {
+    public static void main(String[] args) {
+       int []A={2,2,1,1,1,2,2};
+       int N=A.length;
+
+       int element;
+       int freq=1;
+       element=A[0];
+       for(int i=1;i<N;i++){
+           if(element==A[i]){
+               freq++;
+           }else{
+               if(freq==0){
+                   element=A[i];
+                   freq=1;
+               }else {
+                   freq--;
+               }
+           }
+
+
+       }
+       int count=0;
+       for(int j=0;j<N;j++){
+           if(element==A[j]){
+               count++;
+           }
+       }
+        if(count>N/2){
+            System.out.println("Frequency element found :"+element);
+        }
+
+    }
+}
